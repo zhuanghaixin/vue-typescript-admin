e252d91d483f8476491febe7f5886d2bf33fd704
zhuanghaixin <649308293@qq.com>
2020-05-05 10:08:56 +0800

    feat: 安装echarts 图表封装组件

---
+++ src/components/Charts.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="chart" :style="{width:chartWidth,height:chartHeight}">
+
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component,Vue,Provide,Prop} from 'vue-property-decorator'
+    import echarts from 'echarts'
+    @Component({
+        components:{}
+    })
+    export default class Charts extends Vue{
+        //图表类型 line/bar/pie
+        @Prop({type:String,default:"line"}) readonly chartType!:string
+        //图表数据
+        @Prop(Object) chartData!:string | null
+        //
+        @Provide() chartWidth:string="";
+        @Provide() chartHeight:string="";
+
+        created(){
+            this.generatorWidthAndHeight();
+        }
+        //给图表生成宽度和高度
+        generatorWidthAndHeight(){
+            this.chartWidth=`${document.body.offsetWidth*0.8}px`
+            this.chartHeight=`${document.body.offsetHeight*0.6}px`
+        }
+
+
+
+
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
+ 

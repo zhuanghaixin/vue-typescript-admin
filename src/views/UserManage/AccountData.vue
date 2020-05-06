a7b6d9f3f43458ca59f7c740006d96d996db2bde
zhuanghaixin <649308293@qq.com>
2020-05-06 10:15:16 +0800

    feat: 渲染账户管理页面

--- src/views/UserManage/AccountData.vue
+++ src/views/UserManage/AccountData.vue
@@ -1,20 +1,90 @@
 <template>
     <div class="account-data">
-        AccountData
+        <div class="add-box">
+            <el-button type="primary">新增账户</el-button>
+        </div>
+            <el-table :data="tableData" border style="width:100%">
+                <el-table-column label="角色" widht="180">
+                    <template v-slot:default="scopeProps">
+                        <span>{{scopeProps.row.role}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column label="账户" widht="180">
+                    <template v-slot:default="scopeProps">
+                        <span>{{scopeProps.row.username}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column label="描述" widht="180">
+                    <template v-slot:default="scopeProps">
+                        <span>{{scopeProps.row.des}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column label="操作" width="180">
+                    <template v-slot:default="scopeProps">
+                        <template v-if="scopeProps.row.username!=='admin'">
+                            <el-button size="mini" @click="handleEdit(scopeProps.$index,scopeProps.row)">编辑</el-button>
+                            <el-button size="mini" type="danger"   @click="handleDelete(scopeProps.$index,scopeProps.row)">删除</el-button>
+                        </template>
+
+                    </template>
+                </el-table-column>
+            </el-table>
+
     </div>
 </template>
 
 <script lang="ts">
-    import {Component,Vue} from 'vue-property-decorator'
-    @Component({
-        components:{}
-    })
-    export default class AccountData extends Vue{
+    import {Component, Vue, Provide} from 'vue-property-decorator'
 
+    @Component({
+        components: {}
+    })
+    export default class AccountData extends Vue {
+        @Provide() tableData: any = [];
+
+        created() {
+            this.getData()
+
+        }
+
+        getData() {
+            (this as any).$axios("/api/users/allUsers").then((res: any) => {
+                console.log(res.data.datas)
+                this.tableData = res.data.datas
+
+            })
+        }
+        //编辑表单
+        handleEdit(index:number,row:any){
+            console.log('index')
+            console.log(index)
+            console.log('row')
+            console.log(row)
+
+        }
+
+
+        //删除数据
+        handleDelete(index:number, row:any){
+            console.log(row._id);
+            (this as any).$axios.delete(`/api/profiles/delete/${row._id}`).then((res:any)=>{
+                this.$message({
+                    message:res.data.msg,
+                    type:"success"
+                })
+            })
+            this.tableData.splice(index,1)
+        }
     }
 </script>
 
 <style scoped lang="scss">
-
+    .account-data {
+        height: 100%;
+        overflow: auto;
+        .add-box {
+            margin-bottom: 10px;
+        }
+    }
 </style>
  
